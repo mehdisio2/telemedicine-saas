236806b5f740eb3655bad0747284024cac305fcd
Mahdi Badilou <mehdibadilou7@gmail.com>
2025-11-19 20:23:45 +0100

    add shadcn login-form ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1 className="text-4xl font-extrabold text-white bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500 p-6 rounded-lg shadow-lg text-center">
+        Welcome to the Home Page
+      </h1>
     </div>
   );
 }
---
+++ components/login-form.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+export function LoginForm() {
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    console.log("Logging in:", { email, password })
+  }
+
+  return (
+    <form onSubmit={handleLogin} className="flex flex-col gap-6">
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="text-2xl font-bold">Login to your account</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Enter your email below to login to your account
+          </p>
+        </div>
+
+        <Field>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Input id="email" name="email" type="email" placeholder="m@example.com" required />
+        </Field>
+
+        <Field>
+          <div className="flex items-center">
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <a
+              href="#"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </a>
+          </div>
+          <Input id="password" name="password" type="password" required />
+        </Field>
+
+        <Field>
+          <Button type="submit">Login</Button>
+        </Field>
+
+        <Field>
+          <FieldDescription className="text-center">
+            Don&apos;t have an account?{" "}
+            <a href="#" className="underline underline-offset-4">
+              Sign up
+            </a>
+          </FieldDescription>
+        </Field>
+      </FieldGroup>
+    </form>
+  )
+}
